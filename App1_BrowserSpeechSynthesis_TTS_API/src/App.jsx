7469bcd3b11d4800c4e5e242fc5836fdd6014837
Aman kumar <amankumar@Amans-MacBook-Air.local>
2025-10-03 18:41:33 +0530

    added background color

--- App1_BrowserSpeechSynthesis_TTS_API/src/App.jsx
+++ App1_BrowserSpeechSynthesis_TTS_API/src/App.jsx
@@ -105,7 +105,7 @@ export default function App() {
   };
 
   return (
-    <div style={{ padding: "20px", fontFamily: "Arial" }}>
+    <div style={{ padding: "20px", fontFamily: "Arial", backgroundColor: "yellow" }}>
       <h1>Multilingual Text-to-Speech (SpeechSynthesis API)</h1>
       <p>
         adding new para for feature/adding-line PR
